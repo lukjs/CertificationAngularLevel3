8071b4a1fc113e51c4c06b4128671bc981ad2620
lukjs <lucasl.pro13@gmail.com>
2022-10-07 21:03:00 +0200

    WIP Rework of weather service

--- src/app/current-conditions/current-conditions.component.ts
+++ src/app/current-conditions/current-conditions.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
-import {WeatherService} from "../weather.service";
-import {LocationService} from "../location.service";
-import {Router} from "@angular/router";
+import { Component } from "@angular/core";
+import { WeatherData, WeatherService } from "../weather.service";
+import { LocationService } from "../location.service";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-current-conditions',
-  templateUrl: './current-conditions.component.html',
-  styleUrls: ['./current-conditions.component.css']
+  selector: "app-current-conditions",
+  templateUrl: "./current-conditions.component.html",
+  styleUrls: ["./current-conditions.component.css"],
 })
 export class CurrentConditionsComponent {
+  currentConditions$: Observable<WeatherData>;
 
-  constructor(private weatherService : WeatherService, private locationService : LocationService, private router : Router) {
+  constructor(
+    private weatherService: WeatherService,
+    private locationService: LocationService,
+    private router: Router
+  ) {
+    this.currentConditions$ = this.weatherService.currentConditions;
   }
 
-  getCurrentConditions() {
-    return this.weatherService.getCurrentConditions();
-  }
-
-  showForecast(zipcode : string){
-    this.router.navigate(['/forecast', zipcode])
+  showForecast(zipcode: string) {
+    this.router.navigate(["/forecast", zipcode]);
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,60 +1,83 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Injectable, OnDestroy } from "@angular/core";
+import {
+  BehaviorSubject,
+  from,
+  merge,
+  Observable,
+  Subject,
+  timer,
+  zip,
+} from "rxjs";
+import { map, mergeMap, switchMap, takeUntil, tap } from "rxjs/operators";
 
 import { HttpClient } from "@angular/common/http";
+import { until } from "protractor";
+
+export class WeatherLocation {
+  zipcode?: string;
+  country?: string;
+}
+
+export class WeatherData {}
 
 @Injectable()
-export class WeatherService {
+export class WeatherService implements OnDestroy {
   static URL = "http://api.openweathermap.org/data/2.5";
   static APPID = "5a4b2d457ecbef9eb2a71e480b947604";
-  static ICON_URL =
-    "https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/";
-  private currentConditions = [];
 
-  constructor(private http: HttpClient) {}
+  // private currentConditions = [];
+  private _currentConditions = new BehaviorSubject<
+    Map<WeatherLocation, WeatherData>
+  >(new Map());
 
-  addCurrentConditions(zipcode: string): void {
-    // Here we make a request to get the curretn conditions data from the API. Note the use of backticks and an expression to insert the zipcode
-    this.http
+  private get currentConditionsValue() {
+    return this._currentConditions.value;
+  }
+
+  get currentConditions() {
+    return this._currentConditions
+      .asObservable()
+      .pipe(map((cc) => Array.from(cc).values()));
+  }
+
+  destroy$: Subject<void>;
+  constructor(private http: HttpClient) {
+    const x: Observable<void>[] = [];
+    timer(0, 2000).pipe(takeUntil(this.destroy$), switchMap(merge(x)));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+
+  reloadLocations() {
+    return from([]);
+  }
+
+  getCurrentConditionsOfLocation(
+    location: WeatherLocation
+  ): Observable<WeatherData> {
+    const ccValue = this.currentConditionsValue;
+    return this.http
       .get(
-        `${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`
+        `${WeatherService.URL}/weather?zip=${location.zipcode},us&units=imperial&APPID=${WeatherService.APPID}`
       )
-      .subscribe((data) =>
-        this.currentConditions.push({ zip: zipcode, data: data })
+      .pipe(
+        tap((data) => {
+          ccValue.set(location, data);
+          this._currentConditions.next(ccValue);
+        })
       );
   }
 
+  addCurrentConditions(zipcode: string, country: string): void {
+    this.getCurrentConditionsOfLocation({ zipcode, country });
+  }
+
   removeCurrentConditions(zipcode: string) {
-    for (let i in this.currentConditions) {
-      if (this.currentConditions[i].zip == zipcode)
-        this.currentConditions.splice(+i, 1);
-    }
-  }
-
-  getCurrentConditions(): any[] {
-    return this.currentConditions;
-  }
-
-  getForecast(zipcode: string): Observable<any> {
-    // Here we make a request to get the forecast data from the API. Note the use of backticks and an expression to insert the zipcode
-    return this.http.get(
-      `${WeatherService.URL}/forecast/daily?zip=${zipcode},us&units=imperial&cnt=5&APPID=${WeatherService.APPID}`
-    );
-  }
-
-  getWeatherIcon(id) {
-    if (id >= 200 && id <= 232)
-      return WeatherService.ICON_URL + "art_storm.png";
-    else if (id >= 501 && id <= 511)
-      return WeatherService.ICON_URL + "art_rain.png";
-    else if (id === 500 || (id >= 520 && id <= 531))
-      return WeatherService.ICON_URL + "art_light_rain.png";
-    else if (id >= 600 && id <= 622)
-      return WeatherService.ICON_URL + "art_snow.png";
-    else if (id >= 801 && id <= 804)
-      return WeatherService.ICON_URL + "art_clouds.png";
-    else if (id === 741 || id === 761)
-      return WeatherService.ICON_URL + "art_fog.png";
-    else return WeatherService.ICON_URL + "art_clear.png";
+    // for (let i in this.currentConditions) {
+    //   if (this.currentConditions[i].zip == zipcode)
+    //     this.currentConditions.splice(+i, 1);
+    // }
   }
 }
