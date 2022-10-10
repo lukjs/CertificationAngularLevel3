647e681fc735fa9911fbb9539652a6f22f2dfebd
lukjs <lucasl.pro13@gmail.com>
2022-10-10 17:16:08 +0200

    Add author tag header ⚜

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `<router-outlet></router-outlet>
+  template: `<div
+      style="width: 100%; height: 2em; line-height: 2em; background: rgba(0,0,0,0.2); color: #6e6e6e; margin-bottom:1em"
+    >
+      Made by <strong>Lucas Loignon</strong>
+    </div>
+    <router-outlet></router-outlet>
     <app-message-displayer></app-message-displayer> `,
 })
 export class AppComponent {}
